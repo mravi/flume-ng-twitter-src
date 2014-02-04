61d96a5862f18d9531a2c2eb52e169ab1099ef53
Ravi Magham <maghamravikiran@gmail.com>
2014-02-05 00:50:06 +0530

    adding a full.

--- src/main/java/org/unimonk/flume/source/twitter/TwitterSource.java
+++ src/main/java/org/unimonk/flume/source/twitter/TwitterSource.java
@@ -1,5 +1,5 @@
 /* 
- * Author : Magham Ravi Kiran
+ * Author : Magham Ravi Kiran.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you
 * may not use this file except in compliance with the License. You may
